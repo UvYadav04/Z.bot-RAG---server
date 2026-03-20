246f3eba4b76fe79669707cea78941fb9f984904
Dinesh Yadav <dineshnirban01@gmail.com>
2026-03-20 18:25:58 +0530

    removed spacy

--- utils/queryProcessing.py
+++ utils/queryProcessing.py
@@ -1,7 +1,7 @@
 def queryPreprocessing(query: str):
     query = query.lower().strip()
     # query = correctSpelling(query)
-    query = removeAbbreviations(query)
+    # query = removeAbbreviations(query)
     query = fixContractions(query)
     query = fixSlangs(query)
     return query
@@ -20,29 +20,29 @@ def queryPreprocessing(query: str):
 #     return modified
 
 
-import spacy
-from scispacy.abbreviation import AbbreviationDetector
+# import spacy
+# from scispacy.abbreviation import AbbreviationDetector
 
 # Load SpaCy model
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
 
 # Add abbreviation detector by factory string (SpaCy creates instance internally)
-if "abbreviation_detector" not in nlp.pipe_names:
-    nlp.add_pipe("abbreviation_detector")  # no need to create instance yourself
+# if "abbreviation_detector" not in nlp.pipe_names:
+#     nlp.add_pipe("abbreviation_detector")  # no need to create instance yourself
 
 
 # Function to expand abbreviations
-def removeAbbreviations(query: str):
-    doc = nlp(query)
-    altered_tok = [tok.text for tok in doc]
+# def removeAbbreviations(query: str):
+#     doc = nlp(query)
+#     altered_tok = [tok.text for tok in doc]
 
-    for abrv in doc._.abbreviations:
-        if abrv._.long_form is not None:
-            start, end = abrv.start, abrv.end
-            long_form_tokens = str(abrv._.long_form).split()
-            altered_tok[start:end] = long_form_tokens
+#     for abrv in doc._.abbreviations:
+#         if abrv._.long_form is not None:
+#             start, end = abrv.start, abrv.end
+#             long_form_tokens = str(abrv._.long_form).split()
+#             altered_tok[start:end] = long_form_tokens
 
-    return " ".join(altered_tok)
+#     return " ".join(altered_tok)
 
 
 import contractions
